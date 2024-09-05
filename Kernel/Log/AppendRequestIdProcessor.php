eec3a8dce924903d87ab0059695da0fb8beb0066
wuque <91714264+huangbule2024@users.noreply.github.com>
2024-09-05 09:20:11 +0800

    Add files via upload

---
+++ Kernel/Log/AppendRequestIdProcessor.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Kernel\Log;
+
+use Hyperf\Context\Context;
+use Hyperf\Coroutine\Coroutine;
+use Monolog\LogRecord;
+use Monolog\Processor\ProcessorInterface;
+
+/**
+ * 日志追踪串联
+ * @author hbl
+ */
+class AppendRequestIdProcessor implements ProcessorInterface
+{
+    public const REQUEST_ID = 'log.request.id';
+
+    public function __invoke(array|LogRecord $record)
+    {
+        $record['extra']['request_id'] = Context::getOrSet(self::REQUEST_ID, uniqid());
+        $record['extra']['coroutine_id'] = Coroutine::id();
+        $record['extra']['app_name'] = \Hyperf\Support\env('APP_NAME');
+        return $record;
+    }
+}
